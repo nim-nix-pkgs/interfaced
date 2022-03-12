6796f7ae9c44df6e826b711e2896bd4cfe39c298
hugosenari <hugosenari@gmail.com>
2022-03-12 01:59:18 -0300

    chore: re index interfaced

---
+++ 0_2_0/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Go-like interfaces'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-interfaced-0_2_0.flake = false;
+  inputs.src-interfaced-0_2_0.owner = "andreaferretti";
+  inputs.src-interfaced-0_2_0.ref   = "refs/tags/0.2.0";
+  inputs.src-interfaced-0_2_0.repo  = "interfaced";
+  inputs.src-interfaced-0_2_0.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-interfaced-0_2_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-interfaced-0_2_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Go-like interfaces'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-interfaced-master.flake = false;
+  inputs.src-interfaced-master.owner = "andreaferretti";
+  inputs.src-interfaced-master.ref   = "refs/heads/master";
+  inputs.src-interfaced-master.repo  = "interfaced";
+  inputs.src-interfaced-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-interfaced-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-interfaced-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
